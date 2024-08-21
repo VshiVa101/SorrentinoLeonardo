947780be8e8145f56913a03d68cd1f44471306ca
LeonardoSorrentinoGit <leonardo.sorrentino.lavoro@gmail.com>
2024-08-21 20:37:09 +0200

    Added Projects page and updated navigation

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-//  foglio js presente senza nulla dentro Funzioni interattive future
+document.querySelectorAll('.project').forEach((project) => {
+    project.addEventListener('mouseenter', () => {
+        project.classList.add('hovered');
+    });
+
+    project.addEventListener('mouseleave', () => {
+        project.classList.remove('hovered');
+    });
+});
